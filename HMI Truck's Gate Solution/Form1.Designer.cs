9220b2a04561781fb9acd7c9ed9d27a1d6c78589
primaaldrin <primaaldrinh@gmail.com>
2024-05-28 09:23:27 +0700

    Add project files.

---
+++ HMI Truck's Gate Solution/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace HMI_Truck_s_Gate_Solution
+{
+    partial class Monitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Monitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Monitor";
+            this.Text = "Monitor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+

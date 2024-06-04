0c7b10b44504cf137af736a66b769010ccc01fa5
primaaldrin <primaaldrinh@gmail.com>
2024-06-04 09:22:36 +0700

    What happened?

--- HMI Truck's Gate Solution/Form1.Designer.cs
+++ HMI Truck's Gate Solution/Form1.Designer.cs
@@ -125,13 +125,13 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
